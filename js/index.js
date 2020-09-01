1ab8aecd4184edd1c443161c6a3892319e390c96
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-09-01 18:53:49 -0400

    code updated

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+console.dir(document);
+
+const navLink = document.getElementById("nav-link");
+
+navLink.addEventListener("mouseover", function( event ) {
+    event.target.style.color = "pink"; 
+}
